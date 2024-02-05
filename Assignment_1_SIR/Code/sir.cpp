b9ae5a5111cbead694fda648ccc532244dfb02fc
Lomholy <daniel.lomholt.2000@gmail.com>
2024-02-05 14:44:57 +0100

    Added an ask for initial values to the program

--- Assignment_1_SIR/Code/sir.cpp
+++ Assignment_1_SIR/Code/sir.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
 
+double ask_for_value(string text){
+    cout << text;
+    double value;
+    cin >> value;
+    return value;
+}
+
 int main() {
-    cout << "Hello world - from CPP\n";
+    double beta_factor = ask_for_value("Please enter your beta factor\n");
+    double gamma_factor = ask_for_value("Please enter your gamma factor\n");
+    double population = ask_for_value("Please enter your population factor\n");
+    
+    cout << "You have chosen a beta factor of " << beta_factor <<
+    ", and a gamma factor of "<< gamma_factor << " and a population of " << population << "\n\n";
+
+
+
     return 0;
 }
