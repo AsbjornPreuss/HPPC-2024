1bb4a743bcd171f7be07aaf48fad6bdd32f1c107
Lomholy <daniel.lomholt.2000@gmail.com>
2024-02-07 14:57:13 +0100

    Added figsave and made graph nicer

--- Assignment_1_SIR/Code/pythonread.py
+++ Assignment_1_SIR/Code/pythonread.py
@@ -1,7 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-x, y, z = np.loadtxt('sir_out.txt', skiprows=1, unpack=True)
-plt.plot(x)
-plt.plot(y)
-plt.plot(z)
+x, y, z, t = np.loadtxt('sir_out.txt', skiprows=1, unpack=True)
+plt.plot(t, x, label="Infected")
+plt.plot(t, y, label="Recovered")
+plt.plot(t, z, label="Susceptible")
+plt.xlabel("Time [days]")
+plt.ylabel("Number of people [#]")
+plt.legend()
+plt.tight_layout()
+plt.savefig("../Report/Images/SIR_plot.png", dpi=400)
 plt.show()
+
