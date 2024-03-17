7537ceafe72b1ba51c8e2fe4b91b51a45332774a
E-Cueto <elie@cueto.dk>
2024-03-17 22:01:11 +0100

    Added support for 3d systems. Number of dimensions can be specified at runtime with -n_dims <number of dimensions>. Moved calculation of neighbour indices out of energy calculation and into its own function where it is done once in the beginning, and each spin has its neighbours stored in sys.neighbours. H calculation was giving wrong results. Currently just recalculated at the end as it is not used during the simulation in any case. Current state is no longer stored for each iteration, it was killing the kernel at high nflips, and in the way it was stored it would not have been particularly useful for anything as far as I could tell. visual.ipynb has a list of simulation test results for how many flips one can do at once while having a 2 sigma probability of not choosing 2 neighbouring cells, as a function of grid size in 2d. This may be useful for something I figure.

--- Final_project/Code/seq_hsb.cpp
+++ Final_project/Code/seq_hsb.cpp
@@ -13,6 +13,12 @@ class spin_system {
     int flips = 100; // Number of flips the system will simulate.
     int n_spins = 25; // Number of spins in The system.
     int n_dims = 2; // Number of dimensions the spins are placed in.
+    int n_spins_row; // Number of rows in the square/cube
+
+    int row_offsets[6] = {-1,0,0,1,0,0}; // For calculating neighbor indices in energy calculation
+    int col_offsets[6] = {0,-1,1,0,0,0};
+    int dep_offsets[6] = {0,0,0,0,-1,1};
+
     int x_dist = 10;
     int y_dist = 10;
     int z_dist = 10;
@@ -23,12 +29,12 @@ class spin_system {
     std::string filename = "seq_out.txt"; // Output file.
     std::vector<std::vector<double>> position; // Three by n_spins matrix, defining the spin's 3d position.
     std::vector<std::vector<double>> spin; // Three by n_spins matrix, defining the spin vector for each spin.
-
+    std::vector<std::vector<int>>    neighbours; // 2*n_dims by n_spins matrix, defining the neighbour indices of each cell, so they need only be calculated once.
     spin_system(std::vector <std::string> argument){
     for (long unsigned int i = 1; i<argument.size() ; i += 2){
             std::string arg = argument.at(i);
             if(arg=="-h"){ // Write help
-                std::cout << "Heisenberg_simulation -flips <number of flips performed> -n_spins <number of spins simulated>"
+                std::cout << "Heisenberg_simulation -flips <number of flips performed> -n_spins <number of spins simulated> -n_dims <number of dimensions to simulate>"
                           << " -ofile <filename> \n";
                 exit(0);
                 break;
@@ -36,12 +42,15 @@ class spin_system {
                 flips = std::stoi(argument[i+1]);
             } else if(arg=="-n_spins"){
                 n_spins = std::stoi(argument[i+1]);
+            } else if(arg=="-n_dims"){
+                n_dims = std::stoi(argument[i+1]);
             } else if(arg=="-ofile"){
                 filename = argument[i+1];
             } else{
                 std::cout << "---> error: the argument type is not recognized \n";
             }
         }
+    n_spins_row = pow(float(n_spins),1./float(n_dims));
     }
 };
 
@@ -55,8 +64,8 @@ void generate_positions_box(spin_system &sys){
             sys.position.push_back({i*sys.x_dist, 0, 0});
         break;
     case 2:
-        for (double i=0; i<sqrt(sys.n_spins); i++)
-        for (double j=0; j<sqrt(sys.n_spins); j++)
+        for (double i=0; i<sqrt(sys.n_spins)+2; i++)
+        for (double j=0; j<sqrt(sys.n_spins)+2; j++)
             sys.position.push_back({i*sys.x_dist, j*sys.y_dist, 0});
         break;
     case 3:
@@ -76,35 +85,46 @@ void generate_spin_directions(spin_system &sys){
     
     for (int i = 0; i<sys.n_spins; i++){
         srand(i); // Seed is here to make it perform nicely when comparing to parallel
-        double spin_azimuthal = (double) rand()/RAND_MAX;
+        double spin_azimuthal = (double) rand()/RAND_MAX * M_PI;
         srand(i*rand()); // Seed is here to make it perform nicely when comparing to parallel
-        double spin_polar = (double) rand()/RAND_MAX;
+        double spin_polar = (double) rand()/RAND_MAX * 2. * M_PI;
         
-        sys.spin.push_back({sin(spin_azimuthal*M_PI)*cos(spin_polar*2.*M_PI), 
-                            sin(spin_azimuthal*M_PI)*sin(spin_polar*2.*M_PI),
-                            cos(spin_azimuthal*M_PI)});
+        sys.spin.push_back({sin(spin_azimuthal)*cos(spin_polar), 
+                            sin(spin_azimuthal)*sin(spin_polar),
+                            cos(spin_azimuthal)});
     }   
 };
 
+void generate_neighbours(spin_system &sys){
+    for (int spin = 0; spin<sys.n_spins; spin++){
+        // Find position in square / cube
+        int spin_row = spin/sys.n_spins_row; // Which row the spin is in
+        if(sys.n_dims>2) spin_row = spin_row % sys.n_spins_row;
+        int spin_col = spin%sys.n_spins_row; // Which column the spin is in
+        int spin_dep = spin / (sys.n_spins_row*sys.n_spins_row);
+        
+        // Find indices of neighbours
+        std::vector<int> spin_interactions;
+        for(int i = 0; i < 2* sys.n_dims; i++){
+            spin_interactions.push_back((spin_row + sys.row_offsets[i])%sys.n_spins_row*sys.n_spins_row + 
+                                        (spin_col + sys.col_offsets[i])%sys.n_spins_row + 
+                                        (spin_dep + sys.dep_offsets[i])%sys.n_spins_row * (sys.n_spins_row * sys.n_spins_row));
+            if (spin_interactions[i]<0) spin_interactions[i] *= -1; // Should'nt be necessary, but modulo is not good with - 
+
+        }
+        sys.neighbours.push_back(spin_interactions);
+    }
+}
 // Function that calculates the energy of a single spin in 2d
-double energy_calculation_2d(spin_system &sys, int spin){
-    // Make list of nearest neighbour spin indexes
-    // TODO: Make general for N-nearest neighbours
-    int n_spins_row = sqrt(sys.n_spins);
-    int spin_row = spin/n_spins_row; // Which row the spin is in
-    int spin_col = spin%n_spins_row; // Which column the spin is in
-    int spin_interactions[4] = {(spin_row - 1)%n_spins_row*n_spins_row + (spin_col)%n_spins_row,
-                                 (spin_row)%n_spins_row*n_spins_row + (spin_col - 1)%n_spins_row, 
-                                 (spin_row )%n_spins_row*n_spins_row + (spin_col + 1)%n_spins_row,
-                                 (spin_row + 1)%n_spins_row*n_spins_row + (spin_col)%n_spins_row};
+double energy_calculation_nd(spin_system &sys, int spin){
+    
     double energy = 0;
     double dot_product;
-    for (int i=0; i<4; i++){
+    for (int i=0; i<2*sys.n_dims; i++){
         // Calculate the energy with the nearest neighbour with no corners
-        if (spin_interactions[i]<0) spin_interactions[i] *= -1; // Should'nt be necessary, but modulo is not good with - 
-        dot_product = sys.spin[spin][0]*sys.spin[spin_interactions[i]][0] 
-                        + sys.spin[spin][1]* sys.spin[spin_interactions[i]][1]
-                        + sys.spin[spin][2]* sys.spin[spin_interactions[i]][2];
+        dot_product = sys.spin[spin][0]*sys.spin[sys.neighbours[spin][i]][0] 
+                        + sys.spin[spin][1]* sys.spin[sys.neighbours[spin][i]][1]
+                        + sys.spin[spin][2]* sys.spin[sys.neighbours[spin][i]][2];
         energy += - sys.J/2*dot_product;
     }
     return energy;
@@ -112,8 +132,9 @@ double energy_calculation_2d(spin_system &sys, int spin){
 
 // Calculate the total energy of the system
 void Calculate_h(spin_system& sys){
+    sys.H = 0; // Set H to zero before recalculating it
     for (int i=0; i<sys.n_spins; i++){
-        sys.H += energy_calculation_2d(sys, i)*0.5; // Half the energy, because we calculate on all the spins
+        sys.H += energy_calculation_nd(sys, i)*0.5; // Half the energy, because we calculate on all the spins
     }
 };
 
@@ -123,7 +144,7 @@ void Writeoutput(spin_system& sys, std::ofstream& file){
     for (int i = 0; i<sys.n_spins; i++){
         file << sys.position[i][0] << " " << sys.position[i][1] << " "  << sys.position[i][2] << " "
             << sys.spin[i][0] << " " << sys.spin[i][1] << " "  << sys.spin[i][2] << " "
-            << sys.H
+            << energy_calculation_nd(sys,i)
             << '\n';
     }
 };
@@ -140,27 +161,28 @@ void Simulate(spin_system& sys){
         // Choose a random spin site
         srand(iteration);
         int rand_site = rand()%(sys.n_spins);
-        //std::cout << rand_site << std::endl;
+        
         // Calculate it's current energy
-        current_energy = energy_calculation_2d(sys, rand_site);
+        current_energy = energy_calculation_nd(sys, rand_site);
 
-        // Store it's current state
-        current_state.push_back(sys.spin[rand_site][0]);
-        current_state.push_back(sys.spin[rand_site][1]);
-        current_state.push_back(sys.spin[rand_site][2]);
-        if(verbose) std::cout << current_state[0+3*(iteration+1)] << " " << current_state[1+3*(iteration+1)] << " " << current_state[2+3*(iteration+1)] << std::endl;
-
+        // Store it's current state. 
+        current_state[0] = sys.spin[rand_site][0];
+        current_state[1] = sys.spin[rand_site][1];
+        current_state[2] = sys.spin[rand_site][2];
+        
         // Generate new state
-        spin_azimuthal = (double) rand()/RAND_MAX;
+        spin_azimuthal = (double) rand()/RAND_MAX * M_PI;
         srand(iteration + 1);
-        spin_polar = (double) rand()/RAND_MAX;
-        sys.spin[rand_site] = {sin(spin_azimuthal*M_PI)*cos(spin_polar*2.*M_PI), 
-                            sin(spin_azimuthal*M_PI)*sin(spin_polar*2.*M_PI),
-                            cos(spin_azimuthal*M_PI)};
+        spin_polar = (double) rand()/RAND_MAX * 2. * M_PI;
+        sys.spin[rand_site] = {sin(spin_azimuthal)*cos(spin_polar), 
+                            sin(spin_azimuthal)*sin(spin_polar),
+                            cos(spin_azimuthal)};
+        
         // Calculate if it lowers energy
-        new_energy = energy_calculation_2d(sys, rand_site);
-        //std::cout << "New Energy: " << new_energy << " Old energy: " << current_energy << std::endl;
+        new_energy = energy_calculation_nd(sys, rand_site);
         if (new_energy > current_energy){
+            /* Commenting out this bit for now, since p is always zero anyway. Then it can go a bit faster
+            
             // If not, see if it should be randomised in direction
             //std::cout << "New Energy: " << new_energy << " Old energy: " << current_energy << std::endl;
             probability_of_change = exp(-(new_energy-current_energy)/(Boltzmann*sys.Temperature)); // FIgure out probability of change
@@ -169,18 +191,28 @@ void Simulate(spin_system& sys){
             if (probability_of_change < (double) rand()/RAND_MAX){
                 // If not, revert to old state
                 sys.spin[rand_site] = {
-                    current_state[0+3*(iteration+1)], current_state[1+3*(iteration+1)], current_state[2+3*(iteration+1)]
+                    current_state[0], current_state[1], current_state[2]
                 };
                 no_flips += 1;
+                new_energy = current_energy;
             }
+            */
+            // Remove next few lines when commenting the above back in
+            sys.spin[rand_site] = {
+                    current_state[0], current_state[1], current_state[2]
+                };
+            no_flips += 1;
+            new_energy = current_energy;
         }
         
-        // Change H to represent the total energy of the system
-        sys.H = sys.H - current_energy + new_energy;
+        // Change H to represent the total energy of the system. Gave wrong results. Unclear why. Currently commented out. H is just calculated at the end, as it is not used anywhere in the loop anyway.
+        //sys.H = sys.H - current_energy + new_energy;
     }
+    Calculate_h(sys);
     auto end = std::chrono::steady_clock::now();
     std::cout << "Elapsed Time: " << (end-begin).count() / 1000000000.0 << std::endl;
     std::cout << "Not flipped no. is " << no_flips << std::endl;
+    std::cout << "Total energy: " << sys.H << std::endl;
 }
 //=============================================================================================
 //=========================   MAIN FUNCTION   =================================================
@@ -190,6 +222,7 @@ int main(int argc, char* argv[]){
     spin_system sys({argv, argv+argc});
     generate_positions_box(sys);
     generate_spin_directions(sys);
+    generate_neighbours(sys);
     Calculate_h(sys);
     Simulate(sys);
     std::ofstream file(sys.filename); // open file
