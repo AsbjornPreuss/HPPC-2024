ea8bc3ac24d06bf88ef7969a69f5e7ede52f6a62
Lomholy <daniel.lomholt.2000@gmail.com>
2024-03-18 16:24:37 +0100

    Compiling and running version of parallelisation

--- Final_project/Code/parallel_hsb.cpp
+++ Final_project/Code/parallel_hsb.cpp
@@ -9,7 +9,7 @@
 
 int mpi_size;
 int mpi_rank;
-int nproc_x, nproc_y,nproc_z;
+int nproc_x = 2, nproc_y = 1,nproc_z=1;
 
 bool verbose = false;
 class spin_system {
@@ -104,6 +104,7 @@ class local_spins{
         xlen = local_xlen;
         ylen = local_ylen;
         zlen = local_zlen;
+        
         pad_xlen = xlen+2;
         pad_ylen = ylen+2;
         pad_zlen = zlen+2;
@@ -203,8 +204,9 @@ void Calculate_h(local_spins& sys){
     sys.H = 0; // Set H to zero before recalculating it
     double mag_energy = 0;
     for (int i=0; i<sys.n_spins; i++){
-        sys.H += energy_calculation_nd(sys, i)*0.5; // Half the energy, because we calculate on all the spins
-        mag_energy += sys.spin[i][2]; 
+        int pad_i = sys.index_to_padded_index(i);
+        sys.H += energy_calculation_nd(sys, pad_i)*0.5; // Half the energy, because we calculate on all the spins
+        mag_energy += sys.spin[pad_i][2]; 
     }
     sys.H += sys.B*mag_energy * 0.5; // Half of the magnetization energy is removed above
 };
@@ -222,6 +224,15 @@ void Writeoutput(local_spins& sys, std::ofstream& file){
             << energy_calculation_nd(sys,pad_i)
             << std::endl;
     }
+};
+
+void exchange_ghost_cells(local_spins &local_sys,
+                        MPI_Aint &sdispls, MPI_Aint &rdispls, 
+                        MPI_Datatype &sendtypes, MPI_Datatype &recvtypes,
+                        MPI_Comm cart_comm){
+    int counts[6] = {1,1,1,1,1,1};
+    MPI_Neighbor_alltoallw (local_sys.spin.data(), counts,  &sdispls, &sendtypes,
+                            local_sys.spin.data(), counts,  &rdispls, &recvtypes, cart_comm);
 };
 
 void Simulate(spin_system& sys, local_spins& localsys){
@@ -245,32 +256,31 @@ void Simulate(spin_system& sys, local_spins& localsys){
         // Calculate it's old energy
         old_energy = energy_calculation_nd(localsys, rand_site);
 
-        // Store it's old state. 
-        old_state[0] = sys.spin[rand_site][0];
-        old_state[1] = sys.spin[rand_site][1];
-        old_state[2] = sys.spin[rand_site][2];
-        
+        // Store its old state. 
+        old_state[0] = localsys.spin[rand_site][0];
+        old_state[1] = localsys.spin[rand_site][1];
+        old_state[2] = localsys.spin[rand_site][2];
+      
         // Generate new state
         spin_azimuthal = (double) rand()/RAND_MAX * M_PI;
         srand(mpi_rank*iteration + iteration);
         spin_polar = (double) rand()/RAND_MAX * 2. * M_PI;
-        sys.spin[rand_site] = {sin(spin_azimuthal)*cos(spin_polar), 
+        localsys.spin[rand_site] = {sin(spin_azimuthal)*cos(spin_polar), 
                             sin(spin_azimuthal)*sin(spin_polar),
                             cos(spin_azimuthal)};
         flipped++; 
+
         // Calculate if it lowers energy
         new_energy = energy_calculation_nd(localsys, rand_site);
         if (new_energy > old_energy){
-            // Commenting out this bit for now, since p is always zero anyway. Then it can go a bit faster
-            
             // If not, see if it should be randomised in direction
             if (verbose) std::cout << "New Energy: " << new_energy << " Old energy: " << old_energy << std::endl;
-            probability_of_change = exp(-(new_energy-old_energy)/sys.Temperature); // FIgure out probability of change
+            probability_of_change = exp(-(new_energy-old_energy)/localsys.Temperature); // FIgure out probability of change
             //std::cout << "Change prob: " << probability_of_change << " Exp factor :" << -(new_energy-old_energy)/(Boltzmann*sys.Temperature) << std::endl;
             srand((mpi_rank+1)*(iteration+1)*2);
             if (probability_of_change < (double) rand()/RAND_MAX){
                 // If not, revert to old state
-                sys.spin[rand_site] = {
+                localsys.spin[rand_site] = {
                     old_state[0], old_state[1], old_state[2]
                 };
                 not_flipped++;
@@ -315,53 +325,121 @@ int main(int argc, char* argv[]){
     spin_system global_sys({argv, argv+argc});
 
     //Setup MPI
-    int dims[2] = {nproc_x, nproc_y,nproc_z};
-    int periods[2] = {1,1};
-    int coords[2];
-    MPI_Dims_create(mpi_size, 2, dims);
+    int dims[3] = {nproc_x, nproc_y, nproc_z};
+    int periods[3] = {1,1,1};
+    int coords[3];
+    MPI_Dims_create(mpi_size, 3, dims);
     MPI_Comm cart_comm;
-    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods,
+    MPI_Cart_create(MPI_COMM_WORLD, 3, dims, periods,
             0, &cart_comm);
-    MPI_Cart_coords(cart_comm, mpi_rank, 2, coords);
+    MPI_Cart_coords(cart_comm, mpi_rank, 3, coords);
 
-    int nleft, nright, nbottom, ntop;
-    MPI_Cart_shift(cart_comm, 1,1,&nleft,&nright);
-    MPI_Cart_shift(cart_comm, 0,1,&nbottom,&ntop);
+    int nleft, nright, nbottom, ntop, nfront, nback;
+    MPI_Cart_shift(cart_comm, 2,1,&nleft,&nright);
+    MPI_Cart_shift(cart_comm, 1,1,&nbottom,&ntop);
+    MPI_Cart_shift(cart_comm, 0,1,&nfront,&nback);
+
 
     const long int offset_x = global_sys.xlen * coords[0] / nproc_x -1;
     const long int offset_y = global_sys.ylen * coords[1] / nproc_y -1;
     const long int offset_z = global_sys.zlen * coords[2] / nproc_z -1;
 
-    
     const long int end_x = global_sys.xlen * (coords[0]+1) / nproc_x +1; 
     const long int end_y = global_sys.ylen * (coords[1]+1) / nproc_y +1; 
-    const long int end_z = global_sys.zlen * (coords[2]+1) / nproc_z +1; 
+    const long int end_z = global_sys.zlen * (coords[2]+1) / nproc_z +1;
 
 
     local_spins local_sys(global_sys, 
-            end_x-offset_x, end_y-offset_y, end_z -offset_z,
-            offset_x, offset_y,offset_z);
+            end_x-offset_x, end_y-offset_y, end_z-offset_z,
+            offset_x, offset_y, offset_z);
+    //========================================================================================
+    //========================= START OF GHOST CELL COMMUNICATION SETUP ======================
+    //========================================================================================
 
+    // Define subarray types for ghost cell exchanges
+    /* The following send blocks are defined as follows:
+     * x_type sends to the nearest MPI block in the x direction
+     * y_type sends to the nearest MPI block in the y direction
+     * z_type sends to the nearest MPI block in the z direction
+     * 
+     * The blocks define the parts of data that will be sent in 
+     * MPI_Neigbor_alltoallw.
+     * 
+     * HEAVILY INSPIRED BY https://github.com/essentialsofparallelcomputing/Chapter8/blob/master/GhostExchange/CartExchange3D_Neighbor/CartExchange.cc
+     * LINE 110 AND FORWARD.
+    */
+    MPI_Datatype Vector_type;
+    MPI_Type_contiguous(3,MPI_DOUBLE,&Vector_type);
+    MPI_Type_commit(&Vector_type);
+    const int array_sizes[] = {local_sys.pad_xlen,local_sys.pad_ylen, local_sys.pad_zlen};
+    // send subarrays
+    int subarray_sizes_x[] = { 1,local_sys.ylen,local_sys.zlen};
+    int subarray_x_start[] = {0,1,1};
+    MPI_Datatype x_type;
+    MPI_Type_create_subarray (3, array_sizes, subarray_sizes_x, subarray_x_start,
+                            MPI_ORDER_C, Vector_type, &x_type);
+    MPI_Type_commit(&x_type);
 
+    int subarray_sizes_y[] = {local_sys.xlen, 1, local_sys.zlen};
+    int subarray_y_start[] = {1,0,1};
+    MPI_Datatype y_type;
+    MPI_Type_create_subarray (3, array_sizes, subarray_sizes_y, subarray_y_start,
+                            MPI_ORDER_C, Vector_type, &y_type);
+    MPI_Type_commit(&y_type);
+
+    int subarray_sizes_z[] = { local_sys.xlen, local_sys.ylen,1};
+    int subarray_z_start[] = {1,1,0};
+    MPI_Datatype z_type;
+    MPI_Type_create_subarray (3, array_sizes, subarray_sizes_z, subarray_z_start,
+                            MPI_ORDER_C, Vector_type, &z_type);
+    MPI_Type_commit(&z_type);
+    int xyplane_mult = local_sys.pad_ylen*local_sys.pad_xlen*8*3; //8*3 because datatype is 3 doubles, 
+    int xstride_mult = local_sys.pad_xlen*8*3;
+    // Define displacements of send and receive in bottom top left right.
+    MPI_Aint sdispls[6] = { 8*3,
+                            local_sys.zlen*8*3 ,
+                            xstride_mult,
+                            local_sys.ylen*xstride_mult,
+                            xyplane_mult,
+                            local_sys.pad_zlen*xyplane_mult                          
+                            };
+    MPI_Aint rdispls[6] = {0,
+                            (local_sys.xlen+1)*8*3,
+                            0,
+                            (local_sys.ylen+1)*xstride_mult,
+                            0,
+                            (local_sys.zlen+1)*xyplane_mult,
+                            };
+    
+    MPI_Datatype sendtypes[6] = {z_type, z_type, y_type, y_type, x_type, x_type};
+    MPI_Datatype recvtypes[6] = {z_type, z_type, y_type, y_type, x_type, x_type};
+
+    //========================================================================================
+    //========================= END OF GHOST CELL COMMUNICATION SETUP ========================
+    //========================================================================================
     //Generate system TODO: Done in parallel
     generate_positions_box(local_sys);
     generate_spin_directions(local_sys);
     generate_neighbours(local_sys);
-
     //Magic TODO h as reduction
     Calculate_h(local_sys);
-    Simulate(local_sys);
+    Simulate(global_sys,local_sys);
     
-    if (mpi_rank == 0) std::cout << "Final energy: " << sys.H << std::endl;
+    if (mpi_rank == 0) std::cout << "Final energy: " << global_sys.H << std::endl;
 
     for (int i = 0; i < mpi_size; i++ ){
             if (mpi_rank == i){
-                std::ofstream file(sys.filename); // open file
-                Writeoutput(sys, file);
+                std::ofstream file(local_sys.filename); // open file
+                Writeoutput(local_sys, file);
     }
     MPI_Barrier(MPI_COMM_WORLD);
     }
+    MPI_Type_free(&x_type);
+    MPI_Type_free(&y_type);
+    MPI_Type_free(&z_type);
+    MPI_Type_free(&Vector_type);
 
     MPI_Finalize();
+    
     return 0;
 }
