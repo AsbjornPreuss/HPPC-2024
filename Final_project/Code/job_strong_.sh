f32a7b2d50acb4ae0f899ddbeefc5a93d6c3e4ba
EmilieBerg <emilie.hbv17@gmail.com>
2024-03-22 16:50:52 +0100

    Strong scaling job scripts and data

--- Final_project/Code/job_strong_.sh
+++ Final_project/Code/job_strong_.sh
@@ -3,12 +3,16 @@
 #SBATCH --partition=modi_HPPC
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH -o seq_slurm.out
-##SBATCH --exclusive
+#SBATCH --exclusive
+
 output_file="seq_out";
-#.txt";
+
+Temperature=0.001
+nspins=1259712
+flips=1728000
 #echo "Position_x Position_y Position_z Spin_x Spin_y Spin_z Spin_energy" > $output_file;
-echo $Temperature $nspins $flip;
+echo $Temperature $nspins;
 mpiexec apptainer exec \
 ~/modi_images/ucphhpc/hpc-notebook:latest \
-./seq_hsb --temp $Temperature --nspins $nspins --flips $flip --ofile Data/${output_file}_${Temperature}_${nspins}.txt --magnet 0.05;
+./seq_hsb --temp $Temperature --nspins $nspins --flips $flips --writeout 0 --magnet 0.05;
+
